3c56caea3361bd379925054a5d1c07629406b8e4
cagriyalniz <cagriyalniz@DESKTOP-I5C70L2>
2021-08-07 15:46:43 +0300

    first commit

---
+++ birinci_gun/strings1.py
@@ -0,0 +1,39 @@
+firstStrings = "yağmur"
+secondStrings = "YAĞMUR"
+
+if (firstStrings.upper() == secondStrings.upper()):
+    print("ikisi aynı")
+else:
+    print("ikisi aynı değil")
+
+cumle1 = "'kaşları ne pek kalın, ne pek ince, fakat biraz kısaydı; koyu kumral saçları, köşeli ve oldukça geniş alnını çevreleyerek aşağı doğru uzanıyorlar ve yabankedisinin tüylerine karışıyorlardı sabahattin ali"
+print(len(cumle1))
+
+print(cumle1.split("k"))
+
+ad = input("adınız:")
+print(ad)
+
+sayi1 = input("ilk sayiyi giriniz")
+sayi2 = input("ikinci sayiyi giriniz")
+
+if (int(sayi1) + int(sayi2) >= 50 ):
+    print("sayilarin toplami elliden büyük", int(sayi1) + int(sayi2))
+else:
+    print("sayilarin toplami yeterli küçüklükte: ", int(sayi1) + int(sayi2))
+
+x = 10
+y = 50
+z = 1
+
+z=x
+x=y
+y=z
+print(x)
+print(y)
+
+#python'ın güzelliği:
+a = 30
+b = 99
+a,b = b,a
+print(a,b)
