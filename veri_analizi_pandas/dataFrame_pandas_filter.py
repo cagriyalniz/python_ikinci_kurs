d2ba52c050336cf9880908367189160136375c82
cagriyalniz <cagriyalniz@DESKTOP-I5C70L2>
2021-08-14 14:09:56 +0300

    filter/group

---
+++ veri_analizi_pandas/dataFrame_pandas_filter.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+# filter/group
+filmler = pd.read_csv("imdb-1000.csv")
+#print(filmler)
+print(filmler["title"].head(8))
+print(filmler[["title", "star_rating"]].head(8))
+
+print(filmler[['title', 'star_rating']][filmler['star_rating'] > 9])
+print(filmler[(filmler['star_rating'] > 8) & (filmler['star_rating'] < 8.5)])
+print(filmler[['title', 'star_rating']] [(filmler['star_rating'] > 8.7) & (filmler['star_rating'] < 9)])
+
+print(filmler[(filmler['star_rating'] > 8) & (filmler['star_rating'] < 8.2)][['title', 'star_rating']])
+
+print(filmler.query('star_rating > 8.4')[['title', 'genre', 'star_rating']]) #query adamdır
+
+print("tum filmlerin ort derecesi: " + str(filmler.star_rating.mean()))
+print(filmler.groupby('genre').star_rating.mean())
